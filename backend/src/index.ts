2fd9aa4133bbcdbe0872ed3d9f473817c99e1876
Jacob <jmcentir@gmail.com>
2025-09-18 13:17:41 -0400

    Initial commit - backend working alpha
    Backend successfully receiving request, fetching data from S3, and
    sending that data as a response to initial request

---
+++ backend/src/index.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+// import { fetch } from 'node-fetch';
+
+import { PRESIGNED_S3_URL } from './url';
+
+const app = express()
+
+app.get('/', async (req, res) => {
+  const data = await fetchData()
+  res.send(data)
+})
+
+
+const fetchData = async () => {
+  const s3_response = await fetch(PRESIGNED_S3_URL, {
+    method: 'GET'
+  })
+  console.log('HERE')
+  //const { data, errors } = await s3_response.json()
+  const text = await s3_response.text()
+  console.log(s3_response.headers.get('content-type'))
+  console.log(text);
+  // const r = await s3_response;
+  // console.log(r)
+  // const { data, errors } = r.json()
+	 if (s3_response.ok) {
+     // console.log(data)
+     return text
+	 } else {
+     // console.log('uh oh')
+	 	// handle the graphql errors
+	 	// const error = new Error(
+	 	// 	errors?.map((e: any) => e.message).join('\n') ?? 'unknown',
+	 	// )
+	 	return Promise.reject('lol')
+	 }
+}
+
+app.listen(3000)
